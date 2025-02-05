f9becef1c5145a762e56193a48ad4f34c3ff7391
Alyssandria <sanchez.roniel.work@gmail.com>
2025-02-05 19:42:33 +0800

    feat: login, signup page, http server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,44 @@
 // BACKEND ENTRY POINT
+import http from "node:http";
+import URL from "node:url";
+import fs from "node:fs";
+import path from "node:path";
+
+import { ROUTES, serveFiles } from "./utils.js";
+
+// CONSTANTS
+const PORT = 3000;
+const HOSTNAME = "localhost";
+
+const HTTP_METHODS = {
+  GET: "GET",
+  PUT: "PUT",
+  OPTIONS: "OPTIONS",
+  DELETE: "DELETE",
+  POST: "POST",
+};
+
+const server = http.createServer((req, res) => {
+  const { url, method } = req;
+
+  const paths = URL.parse(url).pathname;
+  // CORS
+  res.setHeader("Control-Access-Allow-Origin", "*");
+  res.setHeader(
+    "Control-Access-Allow-Methods",
+    "GET, PUT, POST, DELETE, OPTIONS"
+  );
+  res.setHeader("Control-Access-Allow-Headers", "Content-Type");
+  if (method === HTTP_METHODS.OPTIONS) {
+    res.statusCode = 204;
+    res.end();
+    return;
+  }
+
+  // SERVE STATIC FILES
+  serveFiles(url, res);
+});
+
+server.listen(PORT, HOSTNAME, () => {
+  console.log(`Server running at ${PORT}`);
+});
---
+++ backend/utils.js
@@ -0,0 +1,49 @@
+import fs from "node:fs";
+import path from "node:path";
+
+export const ROUTES = {
+  SIGNUP: "signup",
+  LOGIN: "login",
+  API: {
+    BASE: "api",
+    USER: "user",
+  },
+};
+
+const FILEPATHS = {
+  "/": "frontend/index.html",
+  "/signup": "frontend/pages/auth/signup/signup.html",
+};
+
+const MIMES = {
+  ".css": "text/css",
+  ".html": "text/html",
+  ".js": "application/javascript",
+  ".svg": "image/svg+xml",
+  ".ico": "image/x-icon",
+};
+
+export const serveFiles = (url, res) => {
+  const cleanUrl = url
+    .split("/")
+    .filter((_, index) => index !== 0)
+    .join("/"); // REMOVE THE FIRST "/" ON THE URL
+  const filePath = FILEPATHS[url] || cleanUrl;
+  const contentType = MIMES[path.extname(filePath)];
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        console.log(err);
+        res.writeHead(404, { "content-type": "text/html" });
+        res.end("<h1>404 Not Found</h1>");
+      } else {
+        console.log(err);
+        res.writeHead(500, { "content-type": "text/plain" });
+        res.end("500 Internal Server Error");
+      }
+    } else {
+      res.writeHead(200, { "content-type": contentType });
+      res.end(content);
+    }
+  });
+};
